3ffa06d0986644eed954b9298b1c0a6ac3b5cf02
Vicbuz <35258492+Vicbuz@users.noreply.github.com>
2021-05-20 15:21:27 +0100

    Add files via upload
    this is the python code to rename samples after the directory they are in

---
+++ rename_samples_WGS.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 12:44:47 2021
+
+@author: Vic Buswell
+This script renames raw fasta files after the directory they are in which is also the sample name,
+ with the suffix .1.fq.gz or .2.fq.gz . This was because the samples came with a name that was not easy to identify 
+ and had no sample number on but the directory did.
+ for example in file 6, contained my sample 6's sequences in forward (_1.fq.gz) and reverse (_2.fq.gz). 
+ But the files were called things like E100011477_L1_B5RDAPIctfRAADAA-615_2.fq.gz
+ this script changes that to 6.2.fq.gz'
+"""
+import os
+import sys
+
+def rename(path):
+        direct = os.listdir(path)
+        suffix = ''
+        for file in direct:
+            suffix = str(file)
+            new_dir = path +'/'+ suffix
+            #print(new_dir)
+            for sample in os.listdir(new_dir):
+                #print(sample)
+                str_sample = str(sample)
+                if '_1.fq' in str_sample:
+                    os.rename(new_dir+'/'+sample, new_dir+'/'+suffix+'.1.fq.gz')
+                elif '_2.fq' in str_sample:
+                    os.rename(new_dir+'/'+sample, new_dir+'/'+suffix+'.2.fq.gz')
+
+
+#rename(r"C:/Users/user/Documents/Python_Scripts/rename_files_test")
+path = str(sys.argv[1])
+rename(path)
