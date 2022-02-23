aa57f25b6dc930923b2676aa3fb7d83a475a66ac
Vicbuz <35258492+Vicbuz@users.noreply.github.com>
2022-02-23 16:21:35 +0000

    Update vcf_allel_freq_colonies_4.py

--- vcf_allel_freq_colonies_4.py
+++ vcf_allel_freq_colonies_4.py
@@ -33,7 +33,7 @@ def get_AD(column):
     return(all_sites)     
         
 
-def read_vcf(path,af_out):
+def read_vcf(path,af_out,SNP_num):
     out_file = open(af_out, 'w')
     with open(path, 'r') as f:
         lines = [l for l in f if not l.startswith('##')]
@@ -41,25 +41,22 @@ def read_vcf(path,af_out):
         io.StringIO(''.join(lines)), delim_whitespace=True
     ).rename(columns={'#CHROM': 'CHROM'})
     print(vcf)
-    # print(vcf.columns)
-    # print(vcf['REF'])
+    SNP_ref = SNP_num - 1
     new_df = vcf.filter(['CHROM','POS','REF','ALT'])
     df_2 = pd.concat([new_df]*2, ignore_index = True)
-    df_2['Ref/Alt']= df_2.iloc[:256152, 4] = 'ref' # row up to the number of SNPs in final file and column 4  is ref works like this -> df.loc[row,column]
-    df_2.iloc[256153:,4] = 'alt' # row from SNPs +1 then column 4 is alt
+    df_2['Ref/Alt']= df_2.iloc[:SNP_ref, 4] = 'ref' # row up to the number of SNPs in final file and column 4 
+    df_2.iloc[SNP_num:,4] = 'alt' # row from SNPs +1 then column 4 is alt
     for column in vcf.iloc[:,9:]: #starting at the first sample column (position9) in the vcf and go to the last column 
         allele_frequencies= get_AD(vcf[column]) # get allele frequencies from the AD field
         af =pd.Series(allele_frequencies) # make the the af a series
         df_2[column]= af.values # add it the the data frame
-        print(df_2)
-    #print(df_2[248669:248674])
-    #df_3 = df_2.iloc[:256152,:2]
     df_2.to_csv(out_file, sep='\t', index =True)# print the new allele frequency data frame out. 
-    #df_3.to_csv(out_file, sep='\t', index = False)
+    
     
 
-read_vcf('E:\\PhD\\NovaSeq\\Filter_colony\\final_filtered_colony_RAD_only.vcf','E:\\PhD\\NovaSeq\\Filter_colony\\allele_freq_RAD_colony.txt')#all_AFs_from_AD_DP_F4_gatk_vcftools_with_basepairs_alt_ref.txt
+#read_vcf('E:\\path\\to\\your\\VCF_file.vcf','E:\\path\\to\\your\\allele_freq_out_file.txt', 15000)
 
-# file_in=str(sys.argv[1])
-# file_out=str(sys.argv[2])
-# read_vcf(file_in, file_out)
+file_in=str(sys.argv[1])
+file_out=str(sys.argv[2])
+snp_num=int(sys.argv[3])
+read_vcf(file_in, file_out, snp_num)
